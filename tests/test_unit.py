2f1400a742f6647b81511763be3980c6502bec8c
riwanahas <120319474+riwanahas@users.noreply.github.com>
2024-04-02 22:50:27 +0200

    Update test_unit.py
    check if data is being read from github secrets

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,6 +2,7 @@
 import pytest
 import requests_mock
 from spotify_reco_by_mood_and_pace.main import get_access_token  
+import os
 
 @pytest.fixture
 def spotify_mock():
@@ -12,3 +13,14 @@ def spotify_mock():
 def test_get_access_token_with_mock(spotify_mock):
     access_token = get_access_token()
     assert access_token == "mocked_token", "Should return the mocked access token"
+# Assuming get_access_token uses these environment variables
+
+def test_spotify_credentials():
+    # Retrieve environment variables
+    client_id = os.getenv("SPOTIFY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+    # Check if the environment variables are not None
+    assert client_id is not None, "SPOTIFY_CLIENT_ID is not set."
+    assert client_secret is not None, "SPOTIFY_CLIENT_SECRET is not set."
+
